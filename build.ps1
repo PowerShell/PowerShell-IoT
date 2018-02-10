fa0cb65dbd0a393aed2fc63eefbe7a63d795605f
Tyler James Leonhardt <tylerl0706@gmail.com>
2018-02-09 17:59:56 -0800

    Buildenv setup
    * remove test results
    
    * only build master and skip README PRs
    
    * add publish and artifacts
    
    * specify artifact in appveyor

---
+++ build.ps1
@@ -0,0 +1,101 @@
+#!/usr/bin/env pwsh
+param(
+    [Parameter()]
+    [switch]
+    $Bootstrap,
+
+    [Parameter()]
+    [switch]
+    $Clean,
+
+    [Parameter()]
+    [switch]
+    $Test
+)
+
+$NeededTools = @{
+    PowerShellCore = "PowerShell Core 6.0.0 or greater"
+    DotNetSdk = "dotnet sdk 2.0 or greater"
+    PowerShellGet = "PowerShellGet latest"
+    InvokeBuild = "InvokeBuild latest"
+}
+
+function needsPowerShellCore () {
+    try {
+        $powershellverison = (pwsh -v)
+    } catch {
+        return $true
+    }
+    return $false
+}
+
+function needsDotNetSdk () {
+    try {
+        $dotnetverison = (dotnet --version)
+    } catch {
+        return $true
+    }
+    return $false
+}
+
+function needsPowerShellGet () {
+    if (Get-Module -ListAvailable -Name PowerShellGet) {
+        return $false
+    }
+    return $true
+}
+
+function needsInvokeBuild () {
+    if (Get-Module -ListAvailable -Name InvokeBuild) {
+        return $false
+    }
+    return $true
+}
+
+function getMissingTools () {
+    $missingTools = @()
+
+    if (needsPowerShellCore) {
+        $missingTools += $NeededTools.PowerShellCore
+    }
+    if (needsDotNetSdk) {
+        $missingTools += $NeededTools.DotNetSdk
+    }
+    if (needsPowerShellGet) {
+        $missingTools += $NeededTools.PowerShellGet
+    }
+    if (needsInvokeBuild) {
+        $missingTools += $NeededTools.InvokeBuild
+    }
+
+    return $missingTools
+}
+
+function hasMissingTools () {
+    return ((getMissingTools).Count -gt 0)
+}
+
+if ($Bootstrap) {
+    $string = "Here is what your environment is missing:`n"
+    $missingTools = getMissingTools
+    if (($missingTools).Count -eq 0) {
+        $string += "* nothing!`n`n Run this script without a flag to build or a -Clean to clean."
+    } else {
+        $missingTools | ForEach-Object {$string += "* $_`n"}
+        $string += "`nAll instructions for installing these tools can be found on PowerShell Editor Services' Github:`n" `
+            + "https://github.com/powershell/PowerShellEditorServices#development"
+    }
+    Write-Host "`n$string`n"
+} elseif(hasMissingTools) {
+    Write-Host "You are missing needed tools. Run './build.ps1 -Bootstrap' to see what they are."
+} else {
+    if($Clean) {
+        Invoke-Build Clean
+    }
+
+    Invoke-Build Build
+
+    if($Test) {
+        Invoke-Build Test
+    }
+}
