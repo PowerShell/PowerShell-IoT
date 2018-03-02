90987d9bffbef9ee94792c568f03b1276517ffcd
Tyler James Leonhardt <tylerl0706@gmail.com>
2018-03-02 10:57:19 -0800

    update build scripts and examples, add helper script (#9)
    * update build scripts and examples, add helper script
    
    * have Read-SoilIsDry return a bool
    
    * add full demo script

---
+++ Examples/Microsoft.PowerShell.IoT.Plant/Microsoft.PowerShell.IoT.Plant.psm1
@@ -0,0 +1,42 @@
+﻿function Read-SoilIsDry
+{
+    $raw = Get-GpioPin -Id 5 -Raw
+    if($raw -eq "High")
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+}
+
+function Set-Light
+{
+    param
+    (
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+        [ValidateSet('On','Off',ignorecase=$true)]
+        [string] $State
+    )
+    if ($State -eq 'On')
+    {
+        $value = "High"
+    }
+    else
+    {
+        $value = "Low"
+    }
+
+    Set-GpioPin -Id 2 -Value $value
+}
+
+function Start-Water
+{
+    Set-GpioPin -Id 0 -Value "Low"
+}
+
+function Stop-Water
+{
+    Set-GpioPin -Id 0 -Value "High"
+}
---
+++ Examples/Microsoft.PowerShell.IoT.SSD1306/Microsoft.PowerShell.IoT.SSD1306.psd1
@@ -0,0 +1,15 @@
+﻿@{
+    GUID="eb74e8da-9ae2-482a-a648-e96550fb8741"
+    Author="Microsoft Corporation"
+    CompanyName="Microsoft Corporation"
+    Copyright="© Microsoft Corporation. All rights reserved."
+    Description='PowerShell module for working with SSD1306 I2C OLED display.'
+    ModuleVersion="1.0.0.0"
+    PowerShellVersion="3.0"
+    FunctionsToExport = @('New-OledDisplay','Set-OledText')
+    DotNetFrameworkVersion = 4.5
+    CmdletsToExport = '*'
+    AliasesToExport = @()
+    NestedModules=@('PSIoT','Microsoft.PowerShell.IoT.SSD1306.psm1')
+    HelpInfoURI = 'https://go.microsoft.com/fwlink/?LinkId=393254'
+}
