f1923b9b59776be0caa0e2780ee919b67382dad3
DanielSSilva <daniel.sobral.silva.9@gmail.com>
2020-04-14 23:48:54 +0100

    initial commit for possible changes on SPI device and data

--- src/Microsoft.PowerShell.IoT/SPI/SPIData.cs
+++ src/Microsoft.PowerShell.IoT/SPI/SPIData.cs
@@ -1,16 +1,34 @@
-public class SPIData
+// public class SPIData
+// {
+//     public int BusId { get; set; }
+//     public int ChipSelectLine { get; set; }
+//     public int Frequency { get; set; }
+//     public byte[] Data { get; set; }
+//     public byte[] Response { get; set; }
+
+//     public SPIData(int busId, int chipSelectLine, int frequency, byte[] data, byte[] response)
+//     {
+//         this.BusId = busId;
+//         this.ChipSelectLine = chipSelectLine;
+//         this.Frequency = frequency;
+//         this.Data = data;
+//         this.Response = response;
+//     }
+// }
+
+using System.Device.Gpio;
+using System.Device.Spi;
+
+public class SPIData : SPIDevice
 {
-    public int BusId { get; set; }
-    public int ChipSelectLine { get; set; }
-    public int Frequency { get; set; }
     public byte[] Data { get; set; }
     public byte[] Response { get; set; }
 
-    public SPIData(int busId, int chipSelectLine, int frequency, byte[] data, byte[] response)
+    public SPIData(SpiDevice device, int busId, int chipSelectLine, int frequency, 
+                   int dataBitLength, SpiMode mode, DataFlow dataFlow, 
+                   PinValue chipSelectLineActiveState, byte[] data, byte[] response
+                  ): base(device, busId, chipSelectLine, frequency, dataBitLength, mode, dataFlow, chipSelectLineActiveState)
     {
-        this.BusId = busId;
-        this.ChipSelectLine = chipSelectLine;
-        this.Frequency = frequency;
         this.Data = data;
         this.Response = response;
     }
--- src/Microsoft.PowerShell.IoT/SPI/Send/SendSPIData.cs
+++ src/Microsoft.PowerShell.IoT/SPI/Send/SendSPIData.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Management.Automation;
 using System.Device.Spi;
+using System.Device.Gpio;
 
 [Cmdlet(VerbsCommunications.Send, "SPIData")]
 public class SendSPIData : Cmdlet
@@ -17,6 +18,18 @@ public class SendSPIData : Cmdlet
     [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, Position = 3)]
     public int Frequency { get; set; }
 
+    [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, Position = 4)]
+    public int DataBitLength { get; set;}
+
+    [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, Position = 5)]
+    public SpiMode Mode { get; set; }
+
+    [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, Position = 6)]
+    public DataFlow DataFlow { get; set; }
+
+    [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, Position = 7)]
+    public PinValue ChipSelectLineActiveState { get; set; }
+
     [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true)]
     public SwitchParameter Raw { get; set; }
 
@@ -29,10 +42,16 @@ public class SendSPIData : Cmdlet
 
     protected override void ProcessRecord()
     {
-        var settings = new SpiConnectionSettings(this.BusId, this.ChipSelectLine);
-        settings.ClockFrequency = this.Frequency;
-        
-        using(var spiDevice = SpiDevice.Create(settings))
+        var settings = new SpiConnectionSettings(this.BusId, this.ChipSelectLine)
+        {
+            ClockFrequency = this.Frequency,
+            DataBitLength = this.DataBitLength,
+            Mode = this.Mode,
+            DataFlow = this.DataFlow,
+            ChipSelectLineActiveState = this.ChipSelectLineActiveState
+        };
+
+        using (var spiDevice = SpiDevice.Create(settings))
         {
             var response = new byte[this.Data.Length];
 
@@ -44,7 +63,10 @@ public class SendSPIData : Cmdlet
             }
             else
             {
-                SPIData spiData = new SPIData(this.BusId, this.ChipSelectLine, this.Frequency, this.Data, response);
+                SPIData spiData = new SPIData(spiDevice, this.BusId, this.ChipSelectLine, this.Frequency, 
+                                                  this.DataBitLength, this.Mode, this.DataFlow, 
+                                                  this.ChipSelectLineActiveState, this.Data, response);
+                //SPIData spiData = new SPIData(this.BusId, this.ChipSelectLine, this.Frequency, this.Data, response);
                 WriteObject(spiData);
             }
         }
