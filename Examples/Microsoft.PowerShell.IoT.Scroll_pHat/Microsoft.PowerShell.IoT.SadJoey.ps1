3c9b4248001161aed0100f5122e5d22e915423af
xtqqczze <xtqqczze@gmail.com>
2020-08-10 16:06:05 +0100

    Update copyright header

--- Examples/Microsoft.PowerShell.IoT.Scroll_pHat/Microsoft.PowerShell.IoT.SadJoey.ps1
+++ Examples/Microsoft.PowerShell.IoT.Scroll_pHat/Microsoft.PowerShell.IoT.SadJoey.ps1
@@ -1,5 +1,6 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
+
 Import-Module Microsoft.PowerShell.IoT
 
 [int]$DeviceAddress = 0x60
